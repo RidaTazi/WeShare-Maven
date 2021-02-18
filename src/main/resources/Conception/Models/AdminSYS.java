73dc5c6ac9bbb4431c7c040bb9201e87b4122544
hamzatamry <hamzatamry@outlook.fr>
2021-02-18 22:58:30 +0100

    Ajout de dossier Models

---
+++ src/main/resources/Conception/Models/AdminSYS.java
@@ -0,0 +1,12 @@
+package DCL;
+
+public class AdminSYS extends User {
+
+	public AdminSYS() {
+		throw new UnsupportedOperationException();
+	}
+
+	public AdminSYS(Object aParameter) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/main/resources/Conception/Models/Donneur.java
@@ -0,0 +1,56 @@
+package DCL;
+
+import java.util.Array;
+import java.util.ArrayList;
+import DCL.Don;
+
+public class Donneur extends User {
+	private String _nom_Donneur;
+	private String _prenom_Donneur;
+	private String _addr_Donneur;
+	public Array<Don> _proposer = new ArrayList<Don>();
+
+	public Donneur() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Donneur(Object aParameter) {
+		throw new UnsupportedOperationException();
+	}
+
+	public String getNom_Donneur() {
+		return this._nom_Donneur;
+	}
+
+	public void setNom_Donneur(String aNom_Donneur) {
+		this._nom_Donneur = aNom_Donneur;
+	}
+
+	public String getAddr_Donneur() {
+		return this._addr_Donneur;
+	}
+
+	public void setAddr_Donneur(String aAddr_Donneur) {
+		this._addr_Donneur = aAddr_Donneur;
+	}
+
+	public static void createDonneur() {
+		throw new UnsupportedOperationException();
+	}
+
+	public static Donneur getDonneur() {
+		throw new UnsupportedOperationException();
+	}
+
+	public static Array<Donneur> filterDonneur() {
+		throw new UnsupportedOperationException();
+	}
+
+	public static void updateDonneur() {
+		throw new UnsupportedOperationException();
+	}
+
+	public static void deleteDonneur() {
+		throw new UnsupportedOperationException();
+	}
+}
