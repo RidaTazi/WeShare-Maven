c6e79815a05b102c8328828225bbe2859d85a322
hamzatamry <hamzatamry@outlook.fr>
2021-02-24 17:59:11 +0100

    check git importation with eclipse

---
+++ bin/src/main/resources/SQL/tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE association (
+    idAssoc INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nomAssoc varchar(100) not null,
+    codeAssoc varchar(20) not null,
+    descAssoc varchar(500) not null,
+    logoAssoc varchar(100),
+    addrAssoc varchar(100),
+    etatInfo int default 1
+);
+
+
+create table membreAssociation(
+    idMembre INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nomMembre varchar(50) not null,
+    prenomMember varchar(50) not null,
+    posteMembre varchar(50) not null,
+    idAssoc INT NOT NULL,
+    CONSTRAINT FK_membre_assoc FOREIGN KEY (idAssoc)
+    REFERENCES association(idAssoc)
+);
+
+
+
+
+
--- src/main/java/Entities/User.java
+++ src/main/java/Entities/User.java
@@ -1,60 +1,42 @@
 package Entities;
 
-public class User {
-//	protected long _iD_User;
-//	protected String _username;
-//	protected String _password;
-//	protected String _email;
-//
-//	public User() {
-//		throw new UnsupportedOperationException();
-//	}
-//
-//	public User(Object aParameter) {
-//		throw new UnsupportedOperationException();
-//	}
-//
-//	public String getUsername() {
-//		return this._username;
-//	}
-//
-//	public void setUsername(String aUsername) {
-//		this._username = aUsername;
-//	}
-//
-//	public String getPassword() {
-//		return this._password;
-//	}
-//
-//	public void setPassword(String aPassword) {
-//		this._password = aPassword;
-//	}
-//
-//	public String getEmail() {
-//		return this._email;
-//	}
-//
-//	public void setEmail(String aEmail) {
-//		this._email = aEmail;
-//	}
-//
-//	public static void createUser() {
-//		throw new UnsupportedOperationException();
-//	}
-//
-//	public static User getUser() {
-//		throw new UnsupportedOperationException();
-//	}
-//
-//	public static Array<User> filterUser() {
-//		throw new UnsupportedOperationException();
-//	}
-//
-//	public static void updateUser() {
-//		throw new UnsupportedOperationException();
-//	}
-//
-//	public static void deleteUser() {
-//		throw new UnsupportedOperationException();
-//	}
+public class User 
+{
+	protected long id_user;
+	protected String username_user;
+	protected String password_user;
+	protected String email_user;
+	
+	public User(long id_user, String username_user, String password_user, String email_user) 
+	{
+		this.id_user = id_user;
+		this.username_user = username_user;
+		this.password_user = password_user;
+		this.email_user = email_user;
+	}
+
+	public String getUsername() {
+		return this.username_user;
+	}
+
+	public void setUsername(String aUsername) {
+		this.username_user = aUsername;
+	}
+
+	public String getPassword() {
+		return this.password_user;
+	}
+
+	public void setPassword(String aPassword) {
+		this.password_user = aPassword;
+	}
+
+	public String getEmail() {
+		return this.email_user;
+	}
+
+	public void setEmail(String aEmail) 
+	{
+		this.email_user = aEmail;
+	}
 }
