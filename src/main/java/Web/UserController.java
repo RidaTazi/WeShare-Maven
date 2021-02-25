76e708b9e1784c83a07be0d0d6e627192e4666eb
hamzatamry <hamzatamry@outlook.fr>
2021-02-25 17:07:56 +0100

    Ajout de httpheaders and Multivalue map

--- src/main/java/Web/UserController.java
+++ src/main/java/Web/UserController.java
@@ -1,26 +1,51 @@
 package Web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import Entities.MembreAssociation;
 
 @Path("/user")
 public class UserController {
 	
+	HashMap<String, String> convertMultiValueMapToMap(MultivaluedMap<String, String> mmap)
+	{
+		HashMap<String, String> hmap = new HashMap<String, String>();
+		for (Map.Entry<String, List<String>> entry : mmap.entrySet()) 
+		{
+		    String key = entry.getKey();
+		    String value = entry.getValue().get(0);
+		   
+		    hmap.put(key, value);
+		}
+		return hmap;
+	}
+	
+	
 	@POST
     @Path("/register")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String register(String data)
+    public Map<String, String> register(String data, @Context  HttpHeaders headers)
     {
-        System.out.println("***********");
-        return "helloWorld";
+		MultivaluedMap<String, String> rh = headers.getRequestHeaders();
+	    return convertMultiValueMapToMap(rh);
     }
 	
 	@POST
