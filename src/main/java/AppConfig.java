da8e643883872a4c52ff6cbd1b1837ba9d12bb57
RidaTazi <riidatazi@gmail.com>
2021-02-27 12:46:14 +0100

    Api with Auth

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,9 +1,12 @@
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-
+import javax.ws.rs.core.Context;
 
 
 @ApplicationPath("/api")
 public class AppConfig extends Application {
-
+    public AppConfig(@Context HttpServletRequest request) {
+        System.out.println("no auth");
+    }
 }
---
+++ src/main/java/AppConfigAuth.java
@@ -0,0 +1,9 @@
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api-auth")
+public class AppConfigAuth extends Application {
+    public AppConfigAuth() {
+        System.out.println("with auth");
+    }
+}
