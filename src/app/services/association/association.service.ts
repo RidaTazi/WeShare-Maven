278e93feaabb46b6647c4e536327fece3533b70e
hamzatamry <58271202+hamzatamry@users.noreply.github.com>
2021-03-02 20:15:55 +0100

    Ajout d'autre indication sur l'authorization

--- src/app/services/association/association.service.ts
+++ src/app/services/association/association.service.ts
@@ -1,12 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from 'src/app/authentication/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AssociationService {
 
-  constructor(private http: HttpClient) { }
+  /*
+    const HEADERS = new HttpHeaders().set("Authorization", "Token " + this.authService.token);
+    this.http.get<RequestInformationVerification[]>(SERVER_URL, { headers: HEADERS })
+  */
+
+  constructor(private authService: AuthService, private http: HttpClient) { }
 
   //methode ici
 }
--- src/app/services/donneur/donneur.service.ts
+++ src/app/services/donneur/donneur.service.ts
@@ -1,12 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from 'src/app/authentication/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DonneurService {
 
-  constructor(private http: HttpClient) { }
+  //registration 
+  //authentication
+
+  //http request (refresh_token,)
+  //token, refresh_token, date_de_creation, date_expiration, user_id
+  //refresh_token
+
+  /*
+    const HEADERS = new HttpHeaders().set("Authorization", "Token " + this.authService.token);
+    this.http.get<RequestInformationVerification[]>(SERVER_URL, { headers: HEADERS })
+  */
+
+  constructor(private authService: AuthService, private http: HttpClient) { }
 
   //methodes ici
 }
