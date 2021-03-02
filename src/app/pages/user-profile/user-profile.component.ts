3933b1c64a83b5a426158b959227e0c1e878f3c8
Delwix <abdelwadoudtm@gmail.com>
2021-03-02 10:21:51 +0000

    added data service for profile page

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { DataService } from './data.service';
+
+
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+
+export class UserProfileComponent implements OnInit {
+  donneur = [{
+    "idDonneur": 1,
+    "dons": null,
+    "nomDonneur": "tazi",
+    "prenomDonneur": "rida",
+    "addrDonneur": "rabat qamra Ahsen dar fl3alam",
+    "roleUser": null,
+    "email": null,
+    "username": null,
+    "password": null,
+    "idUser": null
+  }];
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.sendGetRequest().subscribe((data: any[])=>{
+      console.log(data);
+      this.donneur = data;
+    })  
+
   }
 
 }
