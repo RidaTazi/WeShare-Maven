ee8c294c545dabdaa03ff482cdc7df1eb6cb850e
RidaTazi <riidatazi@gmail.com>
2021-03-03 00:16:41 +0100

    pub-don init

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -8,10 +8,12 @@ import { StockComponent } from 'src/app/pages/stock/stock.component';
 import { AccueilComponent } from 'src/app/pages/accueil/accueil.component';
 import { DonComponent } from 'src/app/pages/don/don.component';
 import { RoleGuard } from 'src/app/authentication/guards/role.guard';
+import { PublicationDonsComponent } from 'src/app/pages/publication-dons/publication-dons.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'publication', component: PublicationComponent,  canActivate: [RoleGuard], data: { roles: ['A'] } },
     { path: 'stock', component: StockComponent, canActivate: [RoleGuard], data: { roles: ['A'] } },
+    { path: 'publication-dons', component: PublicationDonsComponent, canActivate: [RoleGuard], data: { roles: ['A'] } },
     { path: 'accueil', component: AccueilComponent, canActivate: [RoleGuard], data: { roles: ['D'] } },
     { path: 'don', component: DonComponent, canActivate: [RoleGuard], data: { roles: ['D'] } },
     { path: 'user-profile', component: UserProfileComponent},
--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { DataService } from './data.service';
 import { AuthService } from 'src/app/authentication/services/auth.service';
 import { DonneurService } from 'src/app/services/donneur/donneur.service';
 
@@ -15,26 +14,10 @@ import { DonneurService } from 'src/app/services/donneur/donneur.service';
 
 export class UserProfileComponent implements OnInit {
   
-  donneur = {
-    "idDonneur": 1,
-    "dons": null,
-    "nomDonneur": "tazi",
-    "prenomDonneur": "rida",
-    "addrDonneur": "rabat qamra Ahsen dar fl3alam",
-    "roleUser": null,
-    "email": null,
-    "username": null,
-    "password": null,
-    "idUser": null
-  };
 
   constructor(private authService: AuthService, private donneurService: DonneurService) { }
 
   ngOnInit() {
-    this.donneurService.getUserchihaja().subscribe((response: { id: string, username: string}) => {
-     
-    })
-
   }
 
 }
