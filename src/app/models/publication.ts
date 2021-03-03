dbe988364200af2944e574ed5417c32df238686d
RidaTazi <riidatazi@gmail.com>
2021-03-03 19:21:52 +0100

    models directory

---
+++ src/app/models/publication.ts
@@ -0,0 +1,11 @@
+export interface Publication{
+	    id ?: number ;
+        titre ?: string;
+        desc ?: string;
+        date ?: string;
+        type ?: string;
+        etatInfo ?: number;
+        association?: number;
+        dons ?:any[];
+
+}
