7bd71ae0f6259aba6b8053dbdb4accd697bec26c
RidaTazi <riidatazi@gmail.com>
2021-03-04 19:12:10 +0100

    donations of pub onClick

--- src/app/pages/publication-dons/publication-dons.component.ts
+++ src/app/pages/publication-dons/publication-dons.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ExchangeService} from 'src/app/services/exchange.service';
+import { AssociationService } from 'src/app/services/association/association.service';
+import {Don} from 'src/app/models/don';
 
 @Component({
   selector: 'app-publication-dons',
@@ -7,12 +9,17 @@ import {ExchangeService} from 'src/app/services/exchange.service';
   styleUrls: ['./publication-dons.component.css']
 })
 export class PublicationDonsComponent implements OnInit {
-	idPub: number;
+	public dons: Don[]=[]; 
 
-  constructor(private exchange: ExchangeService) {}
+  constructor(private exchange: ExchangeService,private associationService: AssociationService) {}
 
   ngOnInit(): void {
-  	this.idPub=this.exchange.data.pubId;
+  	this.associationService.getDonsByPubId().subscribe(res=>{
+  		this.dons=res;
+  		console.log(this.dons);
+  	},err=>{
+  		alert("Can't fetch Donations, please try later!")
+  	});
   }
 
 }
