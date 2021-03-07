0da2de2674203137862215c05c3aaf074deead67
Delwix <abdelwadoudtm@gmail.com>
2021-03-07 09:08:03 +0000

    association prof

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/authentication/services/auth.service';
 import { DonneurService } from 'src/app/services/donneur/donneur.service';
 import { FormBuilder, FormControl } from '@angular/forms';
+import { Don } from 'src/app/models/don';
 
 @Component({
   selector: 'app-user-profile',
@@ -16,7 +17,8 @@ export class UserProfileComponent implements OnInit {
 
 
   constructor(private authService: AuthService, private donneurService: DonneurService, private formBuilder: FormBuilder) { }
-  
+  public dons:Don[] = [];
+  public nDons: number;
   checkoutForm = this.formBuilder.group({
     id_donneur: this.authService.userId,
     usernameField: new FormControl(),
@@ -39,36 +41,45 @@ export class UserProfileComponent implements OnInit {
   get nom() { return this.checkoutForm.get('nomField').value; }
   get prenom() { return this.checkoutForm.get('prenomField').value; }
   get desc() { return this.checkoutForm.get('descField').value; }
-  
+  get nmbDons() {return this.nDons;}
+
+
   ngOnInit() {
     this.checkoutForm.controls['usernameField'].disable();
-    this.donneurService.getUserchihaja().subscribe((response :{	
-      desc_donneur: string,
-      idDonneur: number,	
-      dons: string,	
-      pays: string,
-      ville: string,
-      nomDonneur: string,	
-      prenomDonneur: string,	
-      addrDonneur: string,	
-      roleUser: string,	
-      email:string,	
-      username: string,	
-      password: string,	
-      idUser: number	
-    })=>{
-      console.log(`role` + this.authService.role);
-      console.log(response)
-      this.checkoutForm.patchValue({descField: response.desc_donneur}),
-      this.checkoutForm.patchValue({emailField: response.email});
-      this.checkoutForm.patchValue({usernameField: response.username});
-      this.checkoutForm.patchValue({addressField: response.addrDonneur});
-      this.checkoutForm.patchValue({nomField: response.nomDonneur});
-      this.checkoutForm.patchValue({prenomField: response.prenomDonneur});
-      this.checkoutForm.patchValue({villeField: response.ville});
-      this.checkoutForm.patchValue({paysField: response.pays});
-    });
-    
+    if(this.authService.role == 'D') {
+      this.donneurService.getUserchihaja().subscribe((response :{	
+        desc_donneur: string,
+        idDonneur: number,	
+        dons: string,	
+        pays: string,
+        ville: string,
+        nomDonneur: string,	
+        prenomDonneur: string,	
+        addrDonneur: string,	
+        roleUser: string,	
+        email:string,	
+        username: string,	
+        password: string,	
+        idUser: number	
+      })=>{
+        console.log(response)
+        this.checkoutForm.patchValue({descField: response.desc_donneur}),
+        this.checkoutForm.patchValue({emailField: response.email});
+        this.checkoutForm.patchValue({usernameField: response.username});
+        this.checkoutForm.patchValue({addressField: response.addrDonneur});
+        this.checkoutForm.patchValue({nomField: response.nomDonneur});
+        this.checkoutForm.patchValue({prenomField: response.prenomDonneur});
+        this.checkoutForm.patchValue({villeField: response.ville});
+        this.checkoutForm.patchValue({paysField: response.pays});
+      });
+      this.donneurService.getUserDons().subscribe(response => {
+        this.dons = response;
+        this.nDons = response.length;
+      },err => console.log(err));
+    }
+    else {
+      //Association
+    }
   }
 
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,4 @@ export const SERVER_ADDRESS = "http://localhost:9090/weshare/api";
 export const environment = {
   production: true
 };
-export const SERVER_ADDRESS = "http://localhost:9090/weshare/api";
+//export const SERVER_ADDRESS = "http://localhost:9090/weshare/api";
